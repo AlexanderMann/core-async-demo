f56bb510f4cef71a34ebdb074ba3a1a87ebb946f
AlexanderMann <mannimal09@gmail.com>
2016-04-07 14:02:34 -0400

    introducing locking mechanisms and callbacks to prevent ordering issues

--- src/core_async_demo/core.clj
+++ src/core_async_demo/core.clj
@@ -7,20 +7,55 @@
   "Waits, prints, flushes so that display more closely represents code.
 
   NOTE: Without the flush, you would only see all of the output when the application exits."
-  [word]
+  [previous-complete? locking-channel is-complete? word]
   (Thread/sleep 1000)
+  (async/<!! previous-complete?)
+  (async/>!! locking-channel (.getName (Thread/currentThread)))
   (print word)
   (flush)
+  (let [lock-value (async/<!! locking-channel)]
+    (when (not= lock-value (.getName (Thread/currentThread)))
+      (throw (Exception. (format "Expected lock-value of: %s got: %s"
+                                 (.getName (Thread/currentThread))
+                                 lock-value)))))
+  (async/close! is-complete?)
   (.getName (Thread/currentThread)))
+
+(defn build-locking-thread
+  "sleepy-print sometimes gets out of order without locking mechanisms like callbacks and channles."
+  [previous-complete? locking-channel input]
+  (let [is-complete? (async/chan)]
+    [is-complete?
+     (async/thread (sleepy-print previous-complete?
+                                 locking-channel
+                                 is-complete?
+                                 input))]))
 
 (defn parallelize-print
   "sleepy-print is slow so to run it faster parallelize-print runs it on multiple cores,
   thus distributing the workload."
   [input]
   (let [words (s/split input #" ")
-        threads (for [word words]
-                  (async/thread (sleepy-print (str word " "))))]
-    (async/<!! (async/into [] (async/merge threads)))))
+        locking-channel (async/chan 1)
+        dummy-thread (async/chan)
+        dummy-starting-thread (async/chan)
+        thread (loop [thread dummy-thread
+                      words words
+                      previous-complete? dummy-starting-thread]
+                 (if (empty? words)
+                   thread
+                   (let [[is-complete? new-thread] (build-locking-thread previous-complete?
+                                                                         locking-channel
+                                                                         (-> words
+                                                                             first
+                                                                             (str " ")))]
+                     (recur (async/merge [thread
+                                          new-thread])
+                            (rest words)
+                            is-complete?))))]
+    (async/close! dummy-starting-thread)
+    (async/close! dummy-thread)
+    (async/<!! (async/into [] thread))))
 
 (defn -main [& _]
   (time
