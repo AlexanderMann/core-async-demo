765fb2798d26adcfa0f534d9f5251498569e9281
AlexanderMann <mannimal09@gmail.com>
2016-04-07 20:52:00 -0400

    adding crdt

--- src/core_async_demo/core.clj
+++ src/core_async_demo/core.clj
@@ -1,61 +1,31 @@
 (ns core-async-demo.core
   (:require [clojure.core.async :as async]
-            [clojure.string :as s])
+            [clojure.string :as s]
+            [core-async-demo.crdt :as crdt])
   (:gen-class))
 
 (defn sleepy-print
-  "Waits, prints, flushes so that display more closely represents code.
-
-  NOTE: Without the flush, you would only see all of the output when the application exits."
-  [previous-complete? locking-channel is-complete? word]
+  "Waits then updates printer."
+  [printer position word]
   (Thread/sleep 1000)
-  (async/<!! previous-complete?)
-  (async/>!! locking-channel (.getName (Thread/currentThread)))
-  (print word)
-  (flush)
-  (let [lock-value (async/<!! locking-channel)]
-    (when (not= lock-value (.getName (Thread/currentThread)))
-      (throw (Exception. (format "Expected lock-value of: %s got: %s"
-                                 (.getName (Thread/currentThread))
-                                 lock-value)))))
-  (async/close! is-complete?)
+  (crdt/idempotent-update printer {:position position
+                                   :v word})
   (.getName (Thread/currentThread)))
-
-(defn build-locking-thread
-  "sleepy-print sometimes gets out of order without locking mechanisms like callbacks and channles."
-  [previous-complete? locking-channel input]
-  (let [is-complete? (async/chan)]
-    [is-complete?
-     (async/thread (sleepy-print previous-complete?
-                                 locking-channel
-                                 is-complete?
-                                 input))]))
 
 (defn parallelize-print
   "sleepy-print is slow so to run it faster parallelize-print runs it on multiple cores,
   thus distributing the workload."
   [input]
   (let [words (s/split input #" ")
-        locking-channel (async/chan 1)
-        dummy-thread (async/chan)
-        dummy-starting-thread (async/chan)
-        thread (loop [thread dummy-thread
-                      words words
-                      previous-complete? dummy-starting-thread]
-                 (if (empty? words)
-                   thread
-                   (let [[is-complete? new-thread] (build-locking-thread previous-complete?
-                                                                         locking-channel
-                                                                         (-> words
-                                                                             first
-                                                                             (str " ")))]
-                     (recur (async/merge [thread
-                                          new-thread])
-                            (rest words)
-                            is-complete?))))]
-    (async/close! dummy-starting-thread)
-    (async/close! dummy-thread)
-    (async/<!! (async/into [] thread))))
+        printer (crdt/create-print-crdt (count words))
+        build-thread-fn (fn [idx word]
+                          (async/thread
+                            (sleepy-print printer idx (str word " "))))
+        threads (map-indexed build-thread-fn words)
+        thread-output (async/<!! (async/into [] (async/merge threads)))]
+    (print (crdt/to-str printer))
+    (flush)
+    thread-output))
 
 (defn -main [& _]
   (time
