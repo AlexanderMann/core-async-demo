cccd1a115e455f205502bcbb3586d25db4e203e2
AlexanderMann <mannimal09@gmail.com>
2016-04-11 20:31:05 -0400

    resetting project

--- src/core_async_demo/core.clj
+++ src/core_async_demo/core.clj
@@ -1,36 +1,21 @@
 (ns core-async-demo.core
-  (:require [clojure.core.async :as async]
-            [clojure.string :as s]
-            [core-async-demo.crdt :as crdt])
+  (:require [clojure.string :as s])
   (:gen-class))
 
-(defn sleepy-print
+(defn sleepy-transform
   "Waits then updates printer."
-  [printer position word]
+  [word]
   (Thread/sleep 1000)
-  (crdt/idempotent-update printer {:position position
-                                   :v word})
-  (.getName (Thread/currentThread)))
+  (s/upper-case word))
 
-(defn parallelize-print
-  "sleepy-print is slow so to run it faster parallelize-print runs it on multiple cores,
-  thus distributing the workload."
-  [input]
-  (let [words (s/split input #" ")
-        printer (crdt/create-print-crdt (count words))
-        build-thread-fn (fn [idx word]
-                          (async/thread
-                            (sleepy-print printer idx (str word " "))))
-        threads (map-indexed build-thread-fn words)
-        thread-output (async/<!! (async/into [] (async/merge threads)))]
-    (print (crdt/to-str printer))
-    (flush)
-    thread-output))
-
-(defn -main [& _]
+(defn main
+  []
   (time
     (let [input "Asynchronous programming is hard, but should it be?"
-          print-threads (parallelize-print input)]
+          result (map sleepy-transform (s/split input #" "))]
       (println)
-      (println print-threads)
+      (println result)
       (println "All done!"))))
+
+(defn -main [& _]
+  (main))
