8e6c0abfc1bbcd8607917812be0f4b0b1a2bedb8
AlexanderMann <mannimal09@gmail.com>
2016-04-11 22:20:50 -0400

    introducing crdt

--- src/core_async_demo/core.clj
+++ src/core_async_demo/core.clj
@@ -1,6 +1,7 @@
 (ns core-async-demo.core
   (:require [clojure.string :as s]
-            [clojure.core.async :as async])
+            [clojure.core.async :as async]
+            [core-async-demo.crdt :as crdt])
   (:gen-class))
 
 (defn sleepy-transform
@@ -9,55 +10,23 @@
   (Thread/sleep 1000)
   (s/upper-case word))
 
-(defn check-lock
-  [locking-channel]
-  (let [lock-value (async/<!! locking-channel)]
-    (when (not= lock-value (.getName (Thread/currentThread)))
-      (throw (Exception. (format "Expected lock-value of: %s got: %s"
-                                 (.getName (Thread/currentThread))
-                                 lock-value))))))
-
-(defn locking-parallel-transform
+(defn parallel-transform
   "An async wrapper for the sleepy-transform."
-  [previous-complete? locking-channel output-channel word]
-  (let [is-complete? (async/chan)]
-    [is-complete?
-     (async/thread
-       (let [retval (sleepy-transform word)]
-         (async/<!! previous-complete?)
-         (async/>!! locking-channel (.getName (Thread/currentThread)))
-         (async/>!! output-channel retval))
-       (check-lock locking-channel)
-       (async/close! is-complete?))]))
+  [crdt-output position word]
+  (async/thread
+    (crdt/idempotent-update
+      crdt-output
+      {:position position
+       :v (sleepy-transform word)})))
 
 (defn coordinate-parallel
   "Coordinate the created threads so that we don't exit before daemon
   threads have completed."
   [input-seq]
-  (let [locking-channel (async/chan 1)
-        dummy-thread (async/chan)
-        dummy-starting-thread (async/chan)
-        output-channel (async/chan (count input-seq))
-        thread (loop [thread dummy-thread
-                      words input-seq
-                      previous-complete? dummy-starting-thread]
-                 (if (empty? words)
-                   thread
-                   (let [[is-complete? new-thread]
-                         (locking-parallel-transform
-                           previous-complete?
-                           locking-channel
-                           output-channel
-                           (first words))]
-                     (recur (async/merge [thread
-                                          new-thread])
-                            (rest words)
-                            is-complete?))))]
-    (async/close! dummy-starting-thread)
-    (async/close! dummy-thread)
-    (async/<!! (async/into [] thread))
-    (async/close! output-channel)
-    (async/<!! (async/into [] output-channel))))
+  (let [crdt-output (crdt/create-crdt-output (count input-seq))
+        thread (map-indexed (partial parallel-transform crdt-output) input-seq)]
+    (async/<!! (async/into [] (async/merge thread)))
+    (crdt/to-str crdt-output)))
 
 (defn main
   []
---
+++ src/core_async_demo/crdt.clj
@@ -0,0 +1,31 @@
+(ns core-async-demo.crdt)
+
+
+(defprotocol CommutativeDT
+  (get-details [this] "Return parameters about as a map")
+  (to-str [this] "Return a string representation")
+  (idempotent-update [this x] "Perform an update which is idempotent to the underlying data-structure"))
+
+(defn- i-update
+  [a position v]
+  (let [current-v (get a position)]
+    (if (or (nil? current-v)
+            (> v current-v))
+      (assoc a position v)
+      a)))
+
+(defrecord CommutativeOutput
+  [l]
+  CommutativeDT
+  (get-details [_]
+    {:n (count @l)})
+  (to-str [_]
+    (str @l))
+  (idempotent-update [this {:keys [position v]}]
+    (swap! l i-update position v)
+    (get-details this)))
+
+(defn create-crdt-output
+  "This is the public mechanism for creating a Print defrecord"
+  [n]
+  (->CommutativeOutput (atom (vec (repeat n nil)))))
